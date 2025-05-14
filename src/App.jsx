eff0ac33bc5c90144f2fb861d441a2d6ba7fc5bc
Wesley Amaro <wesley.amaro.1@gmail.com>
2025-05-14 19:57:04 -0300

    Adicionado botão de excluir e adicionar tarefas

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,72 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
 import React, { useState } from "react";
 import "./App.css";
 import Tasks from "./components/Tasks";
 import AddTasks from "./components/AddTasks";
+import { v4 } from "uuid";
 
 function App() {
   const [tasks, setTasks] = useState([
     {
       id: 1,
-      title: "Estudar programação",
+      title: "Estudar React",
       description:
-        "Estudar programação para se tornar um desenvolvedor full stack",
+        "Estudar React para impulsionar a jornada de se tornar um desenvolvedor full stack",
       isCompleted: false,
     },
     {
       id: 2,
-      title: "Estudar inglês",
-      description: "Estudar inglês para se tornar fluente",
+      title: "Estudar Tailwind Css",
+      description:
+        "Estudar Tailwind para tornar minha estilização mais dinamica",
       isCompleted: false,
     },
     {
       id: 3,
-      title: "Estudar matemática",
+      title: "Estudar TypeScript",
       description:
-        "Estudar matemática para se tornar um desenvolvedor full stack",
+        "Estudar TS para se melhorar o desenvolvimento dos meus projetos",
       isCompleted: false,
     },
   ]);
+
+  function onTaskClick(taskId) {
+    const newTasks = tasks.map((task) => {
+      if (task.id === taskId) {
+        return { ...task, isCompleted: !task.isCompleted };
+      }
+      return task;
+    });
+    setTasks(newTasks);
+  }
+
+  function onDeleteTaksClick(taskId) {
+    const newTasks = tasks.filter((task) => task.id != taskId);
+    setTasks(newTasks);
+  }
+
+  function onAddTaskSubmit(title, description) {
+    const newTask = {
+      id: v4,
+      title:
+        title /* eu podia ter colocado apenas title, sem os :tile, pois o paramentro recebe o "mesmo valor"*/,
+      description: description,
+      isCompleted: false,
+    };
+    setTasks([...tasks, newTask]);
+  }
+
   return (
     <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
-      <div className="w-[500px]">
+      <div className="w-[500px] space-y-4">
         <h1 className="text-3xl text-slate-100 front-bold text-center">
           Gerenciador de Tarefas
         </h1>
-        <Tasks tasks={tasks} />
-        <AddTasks />
+        <AddTasks onAddTaskSubmit={onAddTaskSubmit} />
+        <Tasks
+          tasks={tasks}
+          onTaskClick={onTaskClick}
+          onDeleteTaksClick={onDeleteTaksClick}
+        />
       </div>
     </div>
   );
